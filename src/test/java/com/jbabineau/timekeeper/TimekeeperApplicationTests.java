b1350add17ebf6610a6eb303799f3d082745f340
James Babineau <jbabineau@homesite.com>
2018-07-02 07:35:54 -0400

    first commit

---
+++ src/test/java/com/jbabineau/timekeeper/TimekeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jbabineau.timekeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimekeeperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
